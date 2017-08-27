20d05ca3ede7d3088bfa8ba77fcd4aea746fcdcc
pujie <pw.prayitno@gmail.com>
2017-08-27 21:55:44 +0700

    change about

--- application/views/about.php
+++ application/views/about.php
@@ -76,9 +76,9 @@
                                     <div class="text-muted bootstrap-admin-box-title">License</div>
                                 </div>
                                 <div class="bootstrap-admin-panel-content">
-                                    <p>The MIT License (MIT)</p>
-                                    <p>Copyright © 2013 - Meritoo.pl &lt;github [at] meritoo [dot] pl&gt;</p>
-                                    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+                                    <p>Al Badar</p>
+                                    <p>Copyright © 2017 - najma.web.id &lt;puji [at] najma [dot] web [dot] id &gt;</p>
+                                    <p>Sistem Billing Sekolah</p>
                                     <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
                                     <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
                                 </div>
@@ -86,25 +86,9 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="bs-docs-masthead">
-                                <a href="http://getbootstrap.com" target="_blank">
-                                    <h1>Bootstrap</h1>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6 meritoo-logo">
-                            <a href="http://www.meritoo.pl" target="_blank">
-                                <img src="images/logo-meritoo.png" alt="Meritoo.pl">
-                                <span>Internet software house</span>
-                            </a>
-                        </div>
-                    </div>
-
                     <div class="row row-urls">
                         <div class="col-md-6">
-                            <a href="http://getbootstrap.com" target="_blank">Bootstrap 3.x</a>
+                            <a href="http://pujie.github.io/" target="_blank">Github page</a>
                         </div>
                         <div class="col-md-6">
                             <a href="http://www.najma.web.id" target="_blank">najma.web.id</a>
